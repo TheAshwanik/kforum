c67e821a18c9c28b2f72692a601143758cf212e3
Ashwani Kumar <aryan.ashwani@gmail.com>
2012-10-11 13:40:10 +0200

    bug fixing 11-Oct Rescue on route errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  rescue_from ActionController::RoutingError, :with => :routing_error
+  rescue_from ActiveRecord::RecordNotFound, :with => :routing_error # self defined exception
   
   before_filter :set_cache_buster
   
@@ -22,4 +24,12 @@ class ApplicationController < ActionController::Base
     response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
   end
    
+  
+  protected
+
+    def routing_error(exception)
+      puts exception;
+      redirect_to '/' , notice: 'The page you requested does not exist... 
+      Did you just try to play with the URL....'
+    end 
 end
