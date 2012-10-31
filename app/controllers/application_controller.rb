c2a451cef9c09656d12e70094baed9359eeb4b11
Ashwani Kumar <aryan.ashwani@gmail.com>
2012-10-31 21:37:35 +0100

    added wildcard route for matchin any route and to catch routingError

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  #rescue_from ActionController::RoutingError, :with => :routing_error
+  
+  rescue_from ActionController::RoutingError, :with => :server_error
   #rescue_from ActiveRecord::RecordNotFound, :with => :routing_error # self defined exception
   #rescue_from Exception, :with => :rescue_all_exceptions if RAILS_ENV == 'production'
   
@@ -8,36 +9,32 @@ class ApplicationController < ActionController::Base
 
   before_filter :set_cache_buster
   
-  def admin_required  
-    unless current_user && (current_user.admin == true || current_user.id == 1)  
-      redirect_to '/'  
+    def admin_required  
+      unless current_user && (current_user.admin == true || current_user.id == 1)  
+        redirect_to '/'  
+      end
     end
-  end
-  
-  def admin_or_owner_required(id)  
-    unless current_user.id == id || current_user.admin == true || current_user.id == 1  
-      redirect_to '/'  
+    
+    def admin_or_owner_required(id)  
+      unless current_user.id == id || current_user.admin == true || current_user.id == 1  
+        redirect_to '/'  
+      end
     end
-  end
-  
-  def set_cache_buster
-    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
-    response.headers["Pragma"] = "no-cache"
-    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
-  end
+    
+    def set_cache_buster
+      response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+      response.headers["Pragma"] = "no-cache"
+      response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+    end
    
-  protected
-
-    def routing_error(exception)
-      puts exception;
-      redirect_to '/' , notice: 'The page you requested does not exist... 
-      Did you just try to play with the URL....'
+    def routing_error
+      raise ActionController::RoutingError.new(params[:path])
     end
     
    def server_error(exception)
     ExceptionNotifier::Notifier.exception_notification(request.env, exception).deliver
-    redirect_to '/' , notice: 'The page you requested does not exist... 
-      Did you just try to play with the URL....'
+    redirect_to '/' , notice: 'We are Sorry! Something went wrong, The page you requested does not exist..
+      Or Did you just try to play with the URL....?'
    end
    
 
--- config/routes.rb
+++ config/routes.rb
@@ -83,4 +83,5 @@ KForum::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+  match '*a', :to => 'application#routing_error'
 end
