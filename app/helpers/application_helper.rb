be9c4a15cc8a665f838a8cd894b0d99ed867d5e9
Ashwani Kumar <aryan.ashwani@gmail.com>
2012-11-04 19:40:31 +0100

    Fixed the tag cloud and added meta tags in application

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,31 @@
 module ApplicationHelper
 # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "KForum"
+    base_title = "KForum - For KIS, KPSA , Kabira Developers"
     if page_title.empty?
       base_title
     else
       "#{base_title} | #{page_title}"
     end
   end
+    
+  def meta_keywords(tags = nil)
+    if tags.present?
+      content_for :meta_keywords, tags
+    else
+      content_for?(:meta_keywords) ? [content_for(:meta_keywords), APP_CONFIG['meta_keywords']].join(', ') : APP_CONFIG['meta_keywords']
+    end
+  end
 
-  def admin?  
+  def meta_description(desc = nil)
+    if desc.present?
+      content_for :meta_description, desc
+    else
+      content_for?(:meta_description) ? content_for(:meta_description) : APP_CONFIG['meta_description']
+    end
+  end
+
+  def adminuser?  
     if current_user.admin == true || current_user.id == 1
       return true  
     else 
@@ -26,7 +42,7 @@ module ApplicationHelper
   end
   
   def admin_or_owner?(id)  
-    if (admin? || owner?(id))  
+    if (adminuser? || owner?(id))  
       return true  
     else  
       return false  
