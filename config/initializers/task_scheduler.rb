7252ff31f9fbe59cb4bc3364cd882f23a577d14d
Ashwani Kumar <aryan.ashwani@gmail.com>
2012-11-08 22:58:20 +0100

    added rufus-scheduler2

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -4,11 +4,6 @@ scheduler = Rufus::Scheduler.start_new
 scheduler.every '2m' do
    require "net/http"
    require "uri"
-   if connection.adapter_name == 'PostgreSQL'
-      url = 'http://kforum.ashwani.co.in'
-   else
-      url = 'http://localhost:3000'
-   end
-   
+   url = 'http://kforum.ashwani.co.in'
    Net::HTTP.get_response(URI.parse(url))
 end
